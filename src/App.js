f2d102bf8964d16a3138c3c1ba79f4ec584d1e7e
harshithaj06 <62587773+harshithaj06@users.noreply.github.com>
2020-12-22 15:23:48 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import RedBorderedComponent from './Components/RedBorderedComponent';
+
+class App extends Component {
+  render() {
+    return <RedBorderedComponent/>
+  }
+}
+
+export default App;
