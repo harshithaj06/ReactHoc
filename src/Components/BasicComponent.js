7429b6288d3539a9959831cf3688578b6493f0fd
harshithaj06 <62587773+harshithaj06@users.noreply.github.com>
2020-12-22 15:28:03 +0530

    Update BasicComponent.js

--- src/Components/BasicComponent.js
+++ src/Components/BasicComponent.js
@@ -1,1 +1,10 @@
 
+import React, { Component } from 'react';
+
+class BasicComponent extends Component {
+  render() {
+    return <span> Hi I'm a basic component,</span>
+  }
+}
+
+export default BasicComponent;
